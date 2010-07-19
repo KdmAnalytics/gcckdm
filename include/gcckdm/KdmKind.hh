a1ca2c679c0cacbbb298840a0c6dd6d9499ac0ee
Kyle Girard <kyle@kdmanalytics.com>
2010-07-19 15:12:58 -0400

    Update KdmKind to include more kinds
    Added Add, Subtract, Multiply Divide, Negate

--- include/gcckdm/KdmKind.hh
+++ include/gcckdm/KdmKind.hh
@@ -17,10 +17,20 @@ private:
     {
         //Kdm Group
         KdmKind_Assign,
-    };
+        KdmKind_Add,
+        KdmKind_Subtract,
+        KdmKind_Multiply,
+        KdmKind_Divide,
+        KdmKind_Negate,
+  };
 
 public:
     static const KdmKind Assign() { return KdmKind(KdmKind_Assign,"Assign");}
+    static const KdmKind Add() { return KdmKind(KdmKind_Add,"Add");}
+    static const KdmKind Subtract() { return KdmKind(KdmKind_Subtract,"Subtract");}
+    static const KdmKind Multiply() { return KdmKind(KdmKind_Multiply,"Multiply");}
+    static const KdmKind Divide() { return KdmKind(KdmKind_Divide,"Divide");}
+    static const KdmKind Negate() { return KdmKind(KdmKind_Negate,"Negate");}
 
     int const id() const { return mId; }
     operator int() const { return mId; }

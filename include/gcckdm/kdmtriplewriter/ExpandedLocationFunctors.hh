d98a232d413fd71cf88a1c3787d7291813128e2f
Kyle Girard <kyle@kdmanalytics.com>
2010-07-21 14:12:11 -0400

    Added copywrite and licence information to all source files

--- include/gcckdm/kdmtriplewriter/ExpandedLocationFunctors.hh
+++ include/gcckdm/kdmtriplewriter/ExpandedLocationFunctors.hh
@@ -1,9 +1,23 @@
-/*
- * ExpandedLocationEqual.hh
- *
- *  Created on: Jul 13, 2010
- *      Author: kgirard
- */
+//
+// Copyright (c) 2010 KDM Analytics, Inc. All rights reserved.
+// Date: Jul 13, 2010
+// Author: Kyle Girard <kyle@kdmanalytics.com>
+//
+// This file is part of libGccKdm.
+//
+// Foobar is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// libGccKdm is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with libGccKdm.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 #ifndef GCCKDM_KDMTRIPLEWRITER_EXPANDEDLOCATIONFUNCTORS_HH_
 #define GCCKDM_KDMTRIPLEWRITER_EXPANDEDLOCATIONFUNCTORS_HH_

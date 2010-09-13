7560a0988eeefee3eb9fa22e096a6c3a59f2db0c
Kyle Girard <kyle@kdmanalytics.com>
2010-09-13 12:44:28 -0400

    Add missing Version Header

---
+++ include/gcckdm/GccKdmVersion.hh
@@ -0,0 +1,32 @@
+//
+// Copyright (c) 2010 KDM Analytics, Inc. All rights reserved.
+// Created on: Sep 13, 2010
+// Author: Kyle Girard <kyle@kdmanalytics.com>
+//
+// This file is part of libGccKdm.
+//
+// Foobar is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// libGccKdm is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with libGccKdm.  If not, see <http://www.gnu.org/licenses/>.
+
+
+#ifndef GCCKDMVERSION_HH_
+#define GCCKDMVERSION_HH_
+
+
+namespace gcckdm {
+
+std::string const GccKdmVersion = "0.0.9";
+
+}  // namespace gcckdm
+
+#endif /* GCCKDMVERSION_HH_ */

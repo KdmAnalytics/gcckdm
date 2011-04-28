4e48aa10d1124d9fae225a3b42f6160d48d7a4ce
myusuf3 <yusuf@kdmanalytics.net>
2011-04-28 16:19:38 -0400

    updating the the script for relative paths

--- Release_Linux_x86_64/source/gcckdm/subdir.mk
+++ Release_Linux_x86_64/source/gcckdm/subdir.mk
@@ -26,7 +26,7 @@ CC_DEPS += \
 source/gcckdm/%.o: ../source/gcckdm/%.cc
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++-4.5 -I"/home/myusuf3/Builds/gcckdm/include" -I/usr/lib/gcc/x86_64-linux-gnu/4.5/plugin/include -O3 -Wall -c -fmessage-length=0 -fPIC -Wno-deprecated -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	g++-4.5 -I../include -I/usr/lib/gcc/x86_64-linux-gnu/4.5/plugin/include -O3 -Wall -c -fmessage-length=0 -fPIC -Wno-deprecated -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 

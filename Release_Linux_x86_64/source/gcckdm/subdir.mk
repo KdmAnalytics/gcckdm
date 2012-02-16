1cb420b87d6fd787928a84f87f3f93ce0c3f07e0
Kyle Girard <kyle@kdmanalytics.com>
2012-02-16 14:00:24 -0500

    Update release build for linux 64

--- Release_Linux_x86_64/source/gcckdm/subdir.mk
+++ Release_Linux_x86_64/source/gcckdm/subdir.mk
@@ -6,27 +6,24 @@
 CC_SRCS += \
 ../source/gcckdm/GccKdmPlugin.cc \
 ../source/gcckdm/GccKdmUtilities.cc \
-../source/gcckdm/KdmPredicate.cc \
-../source/gcckdm/KdmType.cc 
+../source/gcckdm/KdmPredicate.cc 
 
 OBJS += \
 ./source/gcckdm/GccKdmPlugin.o \
 ./source/gcckdm/GccKdmUtilities.o \
-./source/gcckdm/KdmPredicate.o \
-./source/gcckdm/KdmType.o 
+./source/gcckdm/KdmPredicate.o 
 
 CC_DEPS += \
 ./source/gcckdm/GccKdmPlugin.d \
 ./source/gcckdm/GccKdmUtilities.d \
-./source/gcckdm/KdmPredicate.d \
-./source/gcckdm/KdmType.d 
+./source/gcckdm/KdmPredicate.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 source/gcckdm/%.o: ../source/gcckdm/%.cc
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++-4.5 -I../include -I/usr/lib/gcc/x86_64-linux-gnu/4.5/plugin/include -O3 -Wall -c -fmessage-length=0 -fPIC -Wno-deprecated -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	g++-4.5 -I../include -I/usr/lib/gcc/x86_64-linux-gnu/4.5/plugin/include -I/usr/lib/x86_64-linux-gnu/gcc/x86_64-linux-gnu/4.5/plugin/include -O3 -Wall -c -fmessage-length=0 -fPIC -Wno-deprecated -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
